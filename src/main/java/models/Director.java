1d96cf62472e9db3c2f8492e9ffececa1901a94b
Jim Bevington <jim.bevington@hotmail.com>
2018-03-16 10:55:46 +0000

    added Director model with getters and setters

---
+++ src/main/java/models/Director.java
@@ -0,0 +1,41 @@
+package models;
+
+import java.util.Set;
+
+public class Director {
+
+    private int id;
+    private String name;
+    private Set<Film> films;
+
+    public Director() {
+    }
+
+    public Director(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Film> getFilms() {
+        return films;
+    }
+
+    public void setFilms(Set<Film> films) {
+        this.films = films;
+    }
+}
