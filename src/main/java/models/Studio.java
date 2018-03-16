0f66943bdc42af72cf0d3e0922157ad434f2c1fa
Jim Bevington <jim.bevington@hotmail.com>
2018-03-16 10:52:52 +0000

    finished Studio model with getters and setters

--- src/main/java/models/Studio.java
+++ src/main/java/models/Studio.java
@@ -5,4 +5,35 @@ public class Studio {
     private int id;
     private String name;
     private Set<Film> films;
+
+    public Studio() {
+    }
+
+    public Studio(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Film> getFilms() {
+        return films;
+    }
+
+    public void setFilms(Set<Film> films) {
+        this.films = films;
+    }
 }
